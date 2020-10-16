d17531980d3adc8655657b432d84e69848025390
Benjamin <benhefc14@gmail.com>
2020-10-16 12:56:14 +0100

    home screen, product screen, ratings, product placeholders

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Rating from './Rating';
+import { Card } from 'react-bootstrap';
+
+const Product = ({ product }) => {
+  return (
+    <div>
+      <Card className="my-3 p-3 rounded">
+        <a href={`/product/${product._id}`}>
+          <Card.Img src={product.image} variant="top" />
+        </a>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews}reviews`}
+          />
+        </Card.Text>
+        <Card.Text as="h3">£{product.price}</Card.Text>
+      </Card>
+    </div>
+  );
+};
+
+export default Product;
